b93143f572d6bda7e59e714aae5aed3400a6cbca
Wdnyana <purwawiadnyana08@gmail.com>
2023-06-02 18:05:18 +0800

    setup self project 2

---
+++ self-project-2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Setup Workk!!</h1>
+      <p className="txt">lorem ipsum</p>
+    </div>
+  );
+}
+
+export default App;
